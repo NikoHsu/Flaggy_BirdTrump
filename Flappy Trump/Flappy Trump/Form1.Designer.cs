f010b3c3cf7c461b94f89a7abf0031ba96f1b77e
Niko <53673991+NikoHsu@users.noreply.github.com>
2024-06-12 10:41:50 +0800

    Add files via upload

---
+++ Flappy Trump/Flappy Trump/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace Flappy_Trump
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.top = new System.Windows.Forms.PictureBox();
+            this.bottom = new System.Windows.Forms.PictureBox();
+            this.ground = new System.Windows.Forms.PictureBox();
+            this.BabyTrump = new System.Windows.Forms.PictureBox();
+            this.scoretext = new System.Windows.Forms.Label();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.top)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bottom)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ground)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BabyTrump)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // top
+            // 
+            this.top.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.top.Image = global::Flappy_Trump.Properties.Resources.Top;
+            this.top.Location = new System.Drawing.Point(473, -55);
+            this.top.Name = "top";
+            this.top.Size = new System.Drawing.Size(118, 287);
+            this.top.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.top.TabIndex = 0;
+            this.top.TabStop = false;
+            // 
+            // bottom
+            // 
+            this.bottom.BackColor = System.Drawing.Color.Magenta;
+            this.bottom.Image = global::Flappy_Trump.Properties.Resources.Button;
+            this.bottom.Location = new System.Drawing.Point(412, 419);
+            this.bottom.Name = "bottom";
+            this.bottom.Size = new System.Drawing.Size(139, 304);
+            this.bottom.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.bottom.TabIndex = 1;
+            this.bottom.TabStop = false;
+            // 
+            // ground
+            // 
+            this.ground.Image = global::Flappy_Trump.Properties.Resources.Wall;
+            this.ground.Location = new System.Drawing.Point(-16, 619);
+            this.ground.Name = "ground";
+            this.ground.Size = new System.Drawing.Size(641, 145);
+            this.ground.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ground.TabIndex = 3;
+            this.ground.TabStop = false;
+            // 
+            // BabyTrump
+            // 
+            this.BabyTrump.Image = global::Flappy_Trump.Properties.Resources.Trump;
+            this.BabyTrump.Location = new System.Drawing.Point(38, 251);
+            this.BabyTrump.Name = "BabyTrump";
+            this.BabyTrump.Size = new System.Drawing.Size(100, 70);
+            this.BabyTrump.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.BabyTrump.TabIndex = 4;
+            this.BabyTrump.TabStop = false;
+            // 
+            // scoretext
+            // 
+            this.scoretext.AutoSize = true;
+            this.scoretext.BackColor = System.Drawing.Color.DarkOrange;
+            this.scoretext.Font = new System.Drawing.Font("Verdana", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.scoretext.Location = new System.Drawing.Point(12, 20);
+            this.scoretext.Name = "scoretext";
+            this.scoretext.Size = new System.Drawing.Size(126, 34);
+            this.scoretext.TabIndex = 5;
+            this.scoretext.Text = "Score:0";
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Interval = 18;
+            this.timer.Tick += new System.EventHandler(this.timerevent);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkCyan;
+            this.BackgroundImage = global::Flappy_Trump.Properties.Resources.bg1;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(617, 757);
+            this.Controls.Add(this.scoretext);
+            this.Controls.Add(this.BabyTrump);
+            this.Controls.Add(this.ground);
+            this.Controls.Add(this.bottom);
+            this.Controls.Add(this.top);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Flaggy Trump (by bilibili 工科狗Niko)";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.keyisdown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.keyisup);
+            ((System.ComponentModel.ISupportInitialize)(this.top)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bottom)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ground)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BabyTrump)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox top;
+        private System.Windows.Forms.PictureBox bottom;
+        private System.Windows.Forms.PictureBox ground;
+        private System.Windows.Forms.PictureBox BabyTrump;
+        private System.Windows.Forms.Label scoretext;
+        private System.Windows.Forms.Timer timer;
+    }
+}
+
---
+++ Flappy Trump/Flappy Trump/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Media;
+using System.Windows.Forms;
+
+namespace Flappy_Trump
+{
+    public partial class Form1 : Form
+    {
+        int flyspeed = 6;
+        int gravity = 6;
+        int score = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            string path = @"D:\Rescarch\VS编程\C#\Flappy Trump\Flappy Trump\Resources\Astronomia2.wav";
+            SoundPlayer bgm = new SoundPlayer(path);
+            bgm.PlayLooping();
+        }
+
+        private void timerevent(object sender, EventArgs e)
+        {
+            BabyTrump.Top += gravity;
+            bottom.Left -= flyspeed;
+            top.Left -= flyspeed;
+           
+
+            scoretext.Text = "score:"+score;
+
+            if (bottom.Left<-100)
+            {
+                bottom.Left = 400;
+                Random r2 = new Random();
+                int a2 = r2.Next(-35, 41);
+                bottom.Top =(290+a2);
+                score++;
+            }
+
+            if (top.Left < -70)
+            {
+                top.Left = 400;
+                Random r1 = new Random();
+                int a1 = r1.Next(-40, 36);
+                top.Top =(-45+ a1);
+                score++;
+            }
+
+            if (BabyTrump.Bounds.IntersectsWith(bottom.Bounds)
+               ||BabyTrump.Bounds.IntersectsWith(top.Bounds)
+               ||BabyTrump.Bounds.IntersectsWith(ground.Bounds)
+               ||BabyTrump.Top<-20
+                )
+            {
+                gameover();       
+            }
+
+            if (score>5)
+            {
+                flyspeed = 10;
+            }
+            if(score>28)
+            {
+                flyspeed = 13;
+                scoretext.Text = "score:" + score+"你太强了！";
+            }
+            if (score > 68)
+            {
+                flyspeed = 18;
+                scoretext.Text = "score:" + score + "难以置信！！";
+            }
+            if (score > 100)
+            {
+                flyspeed = 28;
+                scoretext.Text = "score:" + score + " 无路可退,唯有前进!!!";
+            }
+            if (score > 160)
+            {
+                flyspeed = 34;
+                scoretext.Text = "score:" + score + " 势不可挡!!!";
+            }
+        }
+
+        private void keyisdown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode==Keys.Space)
+            {
+                gravity = -6;
+            }
+        }
+
+        private void keyisup(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space)
+            {
+                gravity = 6;
+            }
+        }
+
+        private void gameover()
+        {
+            timer.Stop();
+            if (MessageBox.Show("游戏失败，是否给 B站“工科狗Niko” 投币以继续游戏？", "提示：得分超过200分后可获得源码！", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                timer.Start();
+                BabyTrump.Top = 200;
+                BabyTrump.Left = 0;
+                bottom.Left += 300;
+                top.Left += 300;
+                gravity = 6;
+            }
+            else
+            {
+                if (score < 200)
+                {
+                    MessageBox.Show("您的选择是明智的，Trump很不靠谱！", "提示：超过200分后可获得源码", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    Environment.Exit(0);
+                }
+                else
+                {
+                    MessageBox.Show("恭喜您超过了200分！源码我放在了：https://github.com/NikoHsu/Bilibili 感谢您对这个小项目的喜爱！", "恭喜！", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    Environment.Exit(0);
+                }
+            }
+        }
+    }
+}
